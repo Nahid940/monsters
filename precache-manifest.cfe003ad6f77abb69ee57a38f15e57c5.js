a5c3cb3531f61370c2f01445bd3dae84c3531d63
nahid940 <nahid940@gmail.com>
2019-12-05 00:16:39 +0600

    Updates

---
+++ precache-manifest.cfe003ad6f77abb69ee57a38f15e57c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f040022b5cd8dd07488e7fc5ec0eed",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "d46154b83e722d629e24",
+    "url": "/monsters/static/css/main.d88d077e.chunk.css"
+  },
+  {
+    "revision": "251a06d8d9982eb5cad4",
+    "url": "/monsters/static/js/2.6de3f2de.chunk.js"
+  },
+  {
+    "revision": "d46154b83e722d629e24",
+    "url": "/monsters/static/js/main.f13f3b3e.chunk.js"
+  },
+  {
+    "revision": "cdf5397aed7a1cc31f48",
+    "url": "/monsters/static/js/runtime-main.a36d0dea.js"
+  }
+]);
